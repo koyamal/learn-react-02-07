96ce68a52e872a86f20b31528794220c714b641d
koyamal <74910401+koyamal@users.noreply.github.com>
2022-11-08 08:39:44 +0900

    Bug code without type

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,21 @@ import axios from "axios";
 import { useState } from "react";
 import "./styles.css";
 
+import { Todo } from "./Todo";
+
 export default function App() {
   const [todos, setTodos] = useState<any>([]);
   const onClickFetchData = () => {
     axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
       setTodos(res.data);
+      console.log(res);
     });
   };
   return (
     <div className="App">
       <button onClick={onClickFetchData}>Get Data</button>
       {todos.map((todo) => {
-        return <p>{todo.title}</p>;
+        return <Todo key={todo.id} title={todo.title} userid={todo.userid} />;
       })}
     </div>
   );
