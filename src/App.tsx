2f8afea9c45c6ce3ea5ef6dff3a0c391d26e0b00
koyamal <74910401+koyamal@users.noreply.github.com>
2022-11-09 08:44:20 +0900

    Use optional chaining(?) in UserProfile.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import "./styles.css";
 import { Todo } from "./Todo";
 import { TodoType } from "./types/todo";
 import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+import { User } from "./types/user";
 
 export default function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -17,8 +19,13 @@ export default function App() {
         console.log(res);
       });
   };
+  const user: User = {
+    name: "Tommy",
+    hobbies: ["Jog", "Game"]
+  };
   return (
     <div className="App">
+      <UserProfile user={user} />
       <Text color="red" fontSize="18px" />
       <button onClick={onClickFetchData}>Get Data</button>
       {todos.map((todo) => {
